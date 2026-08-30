85474b573d745b06c745ad68cc4dea18adc42e4c
DavidCooperWCU <dcooper@wcupa.edu>
2026-08-29 22:50:27 -0400

    sound exercise start

---
+++ source/gdpractice/L5.P1.click_reactions/practices/L5.P1.click_reactions/ball.gd
@@ -0,0 +1,7 @@
+extends Area2D
+
+signal ball_popped(source)
+
+func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		ball_popped.emit(self)
